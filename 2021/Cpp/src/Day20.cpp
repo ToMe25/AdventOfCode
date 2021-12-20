455493937ebcad8c8d3ee872a69340c476654160
ToMe25 <38815969+ToMe25@users.noreply.github.com>
2021-12-20 23:27:15 +0100

    Fix Day 20 C++ part one result

--- 2021/Cpp/src/Day20.cpp
+++ 2021/Cpp/src/Day20.cpp
@@ -40,10 +40,11 @@ void DayRunner<20>::solve(std::ifstream input) {
 	bool *new_image;
 	bool empty_lit = false;
 	uint16_t lit = 0;
-	uint8_t new_height, new_width;
+	uint8_t new_height = current_height;
+	uint8_t new_width = current_width;
 	for (uint8_t i = 0; i < 50; i++) {
-		new_height = current_height + 2;
-		new_width = current_width + 2;
+		new_height += 2;
+		new_width += 2;
 		new_image = new bool[new_height * new_width];
 		for (uint8_t y = 0; y < new_height; y++) {
 			for (uint8_t x = 0; x < new_width; x++) {
@@ -52,7 +53,7 @@ void DayRunner<20>::solve(std::ifstream input) {
 					for (int8_t k = -2; k < 1; k++) {
 						if (y + j >= 0 && y + j < current_height && x + k >= 0
 								&& x + k < current_width) {
-							value += image[(y + j) * current_width + (x + k)]
+							value += image[(y + j) * current_width + x + k]
 									<< (8 - (j + 2) * 3 - k - 2);
 						} else {
 							value += empty_lit << (8 - (j + 2) * 3 - k - 2);
@@ -69,7 +70,7 @@ void DayRunner<20>::solve(std::ifstream input) {
 		image = new_image;
 		empty_lit = empty_lit ? enhancement[511] : enhancement[0];
 
-		if (i == 2) {
+		if (i == 1) {
 			for (uint16_t i = 0; i < current_height * current_width; i++) {
 				if (image[i]) {
 					lit++;
