95c059f3df28abccb5fdd209d54aa5e1393f79ad
ToMe25 <38815969+ToMe25@users.noreply.github.com>
2022-12-02 11:08:51 +0100

    Implement Day 2 part 2 in C++

--- 2022/Cpp/src/Day2.cpp
+++ 2022/Cpp/src/Day2.cpp
@@ -15,7 +15,7 @@ std::string day2part1(std::ifstream input) {
 			continue;
 		}
 
-		score += 1 + line[2] - 'X';
+		score += line[2] - 'X' + 1;
 		if (line[0] - 'A' == line[2] - 'Y') {
 			score += 6;
 		} else if (line[0] == 'C' && line[2] == 'X') {
@@ -28,4 +28,20 @@ std::string day2part1(std::ifstream input) {
 	return std::to_string(score);
 }
 
+std::string day2part2(std::ifstream input) {
+	uint32_t score = 0;
+	std::string line;
+	while (std::getline(input, line)) {
+		if (line.length() == 0) {
+			continue;
+		}
+
+		score += (line[2] - 'X') * 3;
+		score += (line[0] - 'A' + 3 + line[2] - 'Y') % 3 + 1;
+	}
+
+	return std::to_string(score);
+}
+
 bool d2p1 = aoc::registerPart1(2, &day2part1);
+bool d2p2 = aoc::registerPart2(2, &day2part2);
