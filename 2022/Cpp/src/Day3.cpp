c0a1e1c704beb123442f6ac1e24b180efcaa211b
ToMe25 <38815969+ToMe25@users.noreply.github.com>
2022-12-03 13:39:57 +0100

    Optimize Day 3 part 1 in C++ a bit

--- 2022/Cpp/src/Day3.cpp
+++ 2022/Cpp/src/Day3.cpp
@@ -12,18 +12,19 @@ std::string day3part1(std::ifstream input) {
 
 	std::string line;
 	while (std::getline(input, line)) {
-		if (line.length() == 0) {
+		const size_t len = line.length();
+		if (len == 0) {
 			continue;
 		}
 
-		size_t end1 = line.length() / 2;
-		for (size_t i = end1; i < line.length(); i++) {
-			if (std::count(line.begin(), line.begin() + end1, line.at(i)) > 0) {
-				if (line.at(i) >= 'a' && line.at(i) <= 'z') {
-					sum += line.at(i) - 'a' + 1;
-				} else {
-					sum += line.at(i) - 'A' + 27;
-				}
+		const size_t end1 = line.length() / 2;
+		uint64_t first = 0;
+		for (size_t i = 0; i < len; i++) {
+			const char c = line[i];
+			if (i < end1) {
+				first |= (1ll << (c > 'Z' ? c - 'a' : c - 'A' + 26));
+			} else if (first & (1ll << (c > 'Z' ? c - 'a' : c - 'A' + 26))) {
+				sum += (c > 'Z' ? c - 'a' : c - 'A' + 26) + 1;
 				break;
 			}
 		}
@@ -32,4 +33,42 @@ std::string day3part1(std::ifstream input) {
 	return std::to_string(sum);
 }
 
+std::string day3part2(std::ifstream input) {
+	uint32_t sum = 0;
+
+	uint64_t options = 0;
+	size_t ln = 0;
+	std::string line;
+	while (std::getline(input, line)) {
+		if (line.length() == 0) {
+			continue;
+		}
+
+		uint64_t newopts = 0;
+		for (char c : line) {
+			newopts |= (1ll << (c > 'Z' ? c - 'a' : c - 'A' + 26));
+		}
+
+		if (ln % 3 == 0) {
+			options = newopts;
+		} else {
+			options &= newopts;
+			if (ln % 3 == 2) {
+				uint8_t pos = 1;
+				uint64_t mask = 1;
+				while (!(options & mask) && pos < 52) {
+					mask <<= 1;
+					pos++;
+				}
+
+				sum += pos;
+			}
+		}
+		ln++;
+	}
+
+	return std::to_string(sum);
+}
+
 bool d3p1 = aoc::registerPart1(3, &day3part1);
+bool d3p2 = aoc::registerPart2(3, &day3part2);
