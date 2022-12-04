b746505f4458df8c105e9c840746c526f21a1d65
ToMe25 <38815969+ToMe25@users.noreply.github.com>
2022-12-04 13:03:45 +0100

    Implement Day 4 part 2 in C++

--- 2022/Cpp/src/Day4.cpp
+++ 2022/Cpp/src/Day4.cpp
@@ -7,8 +7,9 @@
 
 #include "Main.h"
 
-std::string day4part1(std::ifstream input) {
-	uint32_t useless = 0;
+std::pair<std::string, std::string> day4Combined(std::ifstream input) {
+	uint16_t useless = 0;
+	uint16_t overlapping = 0;
 
 	std::string line;
 	while (std::getline(input, line)) {
@@ -22,16 +23,22 @@ std::string day4part1(std::ifstream input) {
 
 		const int start1 = std::stoi(line.substr(0, first));
 		const int end1 = std::stoi(line.substr(first + 1, comma - first - 1));
-		const int start2 = std::stoi(line.substr(comma + 1, second - comma - 1));
+		const int start2 = std::stoi(
+				line.substr(comma + 1, second - comma - 1));
 		const int end2 = std::stoi(line.substr(second + 1));
 		if (start1 >= start2 && end1 <= end2) {
 			useless++;
 		} else if (start1 <= start2 && end1 >= end2) {
 			useless++;
 		}
+
+		if ((start1 >= start2 && start1 <= end2)
+				|| (start2 >= start1 && start2 <= end1)) {
+			overlapping++;
+		}
 	}
 
-	return std::to_string(useless);
+	return {std::to_string(useless), std::to_string(overlapping)};
 }
 
-bool d4p1 = aoc::registerPart1(4, &day4part1);
+bool d4c = aoc::registerCombined(4, &day4Combined);
