4820c5b899fa0140f58d9c6f22aa042052ab3bff
ToMe25 <38815969+ToMe25@users.noreply.github.com>
2022-12-05 15:30:36 +0100

    Implement Day 5 part 1 in C++

---
+++ 2022/Cpp/src/Day5.cpp
@@ -0,0 +1,70 @@
+/*
+ * Day5.cpp
+ *
+ *  Created on: Dec 5, 2022
+ *      Author: ToMe25
+ */
+
+#include "Main.h"
+#include <sstream>
+
+std::string day5part1(std::ifstream input) {
+	std::vector<std::vector<uint8_t>> crates;
+	std::string line;
+	bool reversed = false;
+	while (std::getline(input, line)) {
+		if (line.length() < 3) {
+			continue;
+		} else if (line.find('[') != std::string::npos) {
+			for (size_t i = 1; i < line.length() + 4; i += 4) {
+				if (!std::isalnum(line[i])) {
+					continue;
+				}
+
+				for (size_t j = crates.size(); j <= (i - 1) / 4; j++) {
+					crates.push_back(std::vector<uint8_t>());
+				}
+				crates[(i - 1) / 4].push_back(line[i]);
+			}
+		} else if (line.rfind("move", 0) == 0) {
+			if (!reversed) {
+				for (std::vector<uint8_t> &stack : crates) {
+					std::reverse(stack.begin(), stack.end());
+				}
+				reversed = true;
+			}
+
+			std::istringstream in(line);
+			std::vector<std::string> tokens;
+			std::string tok;
+			while (in >> tok) {
+				tokens.push_back(tok);
+			}
+
+			if (tokens.size() < 6) {
+				std::cerr << "Received invalid move line \"" << line << "\"."
+						<< std::endl;
+				continue;
+			}
+
+			const uint32_t count = std::stoi(tokens[1]);
+			const uint32_t start = std::stoi(tokens[3]) - 1;
+			const uint32_t target = std::stoi(tokens[5]) - 1;
+			for (size_t i = 0; i < count; i++) {
+				crates[target].push_back(*crates[start].rbegin());
+				crates[start].pop_back();
+			}
+		}
+	}
+
+	std::ostringstream result;
+	for (size_t i = 0; i < crates.size(); i++) {
+		if (crates[i].size() > 0) {
+			result << *crates[i].rbegin();
+		}
+	}
+
+	return result.str();
+}
+
+bool d5p1 = aoc::registerPart1(5, &day5part1);
