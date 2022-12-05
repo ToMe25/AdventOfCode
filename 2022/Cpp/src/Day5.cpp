b9d855cb2d64f4f9010e577fd65dc8a3569570c7
ToMe25 <38815969+ToMe25@users.noreply.github.com>
2022-12-05 15:39:33 +0100

    Implement Day 5 part 2 in C++

--- 2022/Cpp/src/Day5.cpp
+++ 2022/Cpp/src/Day5.cpp
@@ -67,4 +67,62 @@ std::string day5part1(std::ifstream input) {
 	return result.str();
 }
 
+std::string day5part2(std::ifstream input) {
+	std::vector<std::string> crates;
+	std::string line;
+	bool reversed = false;
+	while (std::getline(input, line)) {
+		if (line.length() < 3) {
+			continue;
+		} else if (line.find('[') != std::string::npos) {
+			for (size_t i = 1; i < line.length() + 4; i += 4) {
+				if (!std::isalnum(line[i])) {
+					continue;
+				}
+
+				for (size_t j = crates.size(); j <= (i - 1) / 4; j++) {
+					crates.push_back(std::string());
+				}
+				crates[(i - 1) / 4] += line[i];
+			}
+		} else if (line.rfind("move", 0) == 0) {
+			if (!reversed) {
+				for (std::string &stack : crates) {
+					std::reverse(stack.begin(), stack.end());
+				}
+				reversed = true;
+			}
+
+			std::istringstream in(line);
+			std::vector<std::string> tokens;
+			std::string tok;
+			while (in >> tok) {
+				tokens.push_back(tok);
+			}
+
+			if (tokens.size() < 6) {
+				std::cerr << "Received invalid move line \"" << line << "\"."
+						<< std::endl;
+				continue;
+			}
+
+			const uint32_t count = std::stoi(tokens[1]);
+			const uint32_t start = std::stoi(tokens[3]) - 1;
+			const uint32_t target = std::stoi(tokens[5]) - 1;
+			crates[target] += crates[start].substr(crates[start].length() - count);
+			crates[start] = crates[start].substr(0, crates[start].length() - count);
+		}
+	}
+
+	std::ostringstream result;
+	for (size_t i = 0; i < crates.size(); i++) {
+		if (crates[i].size() > 0) {
+			result << *crates[i].rbegin();
+		}
+	}
+
+	return result.str();
+}
+
 bool d5p1 = aoc::registerPart1(5, &day5part1);
+bool d5p2 = aoc::registerPart2(5, &day5part2);
