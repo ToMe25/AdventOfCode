25a9409edb692776af2d52a92f20792facaf3a77
ToMe25 <38815969+ToMe25@users.noreply.github.com>
2022-12-08 17:22:32 +0100

    Implement Day 8 part 2 in C++

--- 2022/Cpp/src/Day8.cpp
+++ 2022/Cpp/src/Day8.cpp
@@ -7,7 +7,7 @@
 
 #include "Main.h"
 
-std::string day8part1(std::ifstream input) {
+std::pair<std::string, std::string> day8combined(std::ifstream input) {
 	std::vector<std::vector<uint8_t>> map;
 	std::string line;
 	size_t ln = 0;
@@ -25,63 +25,79 @@ std::string day8part1(std::ifstream input) {
 	}
 
 	size_t visible = 0;
+	size_t best_scenic = 0;
 	for (size_t y = 0; y < map.size(); y++) {
 		for (size_t x = 0; x < map[y].size(); x++) {
 			const uint8_t c = map[y][x];
 			bool hidden = false;
+			size_t scenic = 1;
+			bool vis = false;
 			for (int64_t x1 = x - 1; x1 >= 0; x1--) {
 				if (map[y][x1] >= c) {
 					hidden = true;
+					scenic *= x - x1;
 					break;
 				}
 			}
 
 			if (!hidden) {
-				visible++;
-				continue;
+				vis = true;
+				scenic *= x;
 			}
 
 			hidden = false;
 			for (int64_t x1 = x + 1; x1 < map[y].size(); x1++) {
 				if (map[y][x1] >= c) {
 					hidden = true;
+					scenic *= x1 - x;
 					break;
 				}
 			}
 
 			if (!hidden) {
-				visible++;
-				continue;
+				vis = true;
+				scenic *= map[y].size() - x - 1;
 			}
 
 			hidden = false;
 			for (int64_t y1 = y - 1; y1 >= 0; y1--) {
 				if (map[y1][x] >= c) {
 					hidden = true;
+					scenic *= y - y1;
 					break;
 				}
 			}
 
 			if (!hidden) {
-				visible++;
-				continue;
+				vis = true;
+				scenic *= y;
 			}
 
 			hidden = false;
 			for (int64_t y1 = y + 1; y1 < map.size(); y1++) {
 				if (map[y1][x] >= c) {
 					hidden = true;
+					scenic *= y1 - y;
 					break;
 				}
 			}
 
 			if (!hidden) {
+				vis = true;
+				scenic *= map.size() - y - 1;
+			}
+
+			if (vis) {
 				visible++;
+			}
+
+			if (best_scenic < scenic) {
+				best_scenic = scenic;
 			}
 		}
 	}
 
-	return std::to_string(visible);
+	return {std::to_string(visible), std::to_string(best_scenic)};
 }
 
-bool d8p1 = aoc::registerPart1(8, &day8part1);
+bool d8c = aoc::registerCombined(8, &day8combined);
