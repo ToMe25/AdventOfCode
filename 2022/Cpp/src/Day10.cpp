4802d4e5a4225ea7aecdbe061d01c9dc7b676cec
ToMe25 <38815969+ToMe25@users.noreply.github.com>
2022-12-10 21:52:08 +0100

    Implement Day 10 part 2 in C++

--- 2022/Cpp/src/Day10.cpp
+++ 2022/Cpp/src/Day10.cpp
@@ -34,4 +34,34 @@ std::string day10part1(std::ifstream input) {
 	return std::to_string(score);
 }
 
+std::string day10part2(std::ifstream input) {
+	int64_t registerX = 1;
+	std::string image = "\n";
+	uint16_t clock = 0;
+	std::string line;
+	while(std::getline(input, line)) {
+		if (line.length() == 0) {
+			continue;
+		}
+		image += (clock % 40 >= registerX - 1 && clock % 40 <= registerX + 1) ? '#' : '.';
+		clock++;
+		if (clock % 40 == 0) {
+			image += '\n';
+		}
+
+		if (line == "noop") {
+			// Do nothing
+		} else if (line.substr(0, 4) == "addx") {
+			image += (clock % 40 >= registerX - 1 && clock % 40 <= registerX + 1) ? '#' : '.';
+			clock++;
+			if (clock % 40 == 0) {
+				image += '\n';
+			}
+			registerX += std::stoi(line.substr(5));
+		}
+	}
+	return image;
+}
+
 bool d10p1 = aoc::registerPart1(10, &day10part1);
+bool d10p2 = aoc::registerPart2(10, &day10part2);
