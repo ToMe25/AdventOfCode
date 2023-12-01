53b300d193f22c939b96929cf56c093059988aca
ToMe25 <38815969+ToMe25@users.noreply.github.com>
2023-12-01 14:58:00 +0100

    Implement day 1 part 2 in rust

--- 2023/Rust/src/days/day1.rs
+++ 2023/Rust/src/days/day1.rs
@@ -18,17 +18,58 @@ impl DayRunner for Day1Runner {
         let input = get_input_file(1)?;
         let mut sum: u32 = 0;
         for (i, line) in fs::read_to_string(input)?.lines().enumerate() {
-            let mut first: Option<u8> = None;
-            let mut last: u8 = 0;
-            for c in line.chars() {
-                if c >= '0' && c <= '9' {
-                    let _ = first.get_or_insert(c as u8 - b'0');
-                    last = c as u8 - b'0';
-                }
-            }
+            let mut chars = line.matches(|c: char| c.is_digit(10));
+            let first = chars.next().map(|s| s.parse::<u8>().unwrap());
+            let last = chars
+                .last()
+                .map(|s| s.parse::<u8>().unwrap())
+                .unwrap_or(first.unwrap_or(0));
 
             if let Some(j) = first {
                 sum += (j * 10 + last) as u32;
+            } else {
+                eprintln!("Line {}({}) didn't contain a single digit.", i, line);
+            }
+        }
+
+        Ok(Some(sum.to_string()))
+    }
+
+    fn part2(&self) -> Result<Option<String>, Box<dyn Error>> {
+        let numbers = [
+            "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+        ];
+        let input = get_input_file(1)?;
+        let mut sum: u32 = 0;
+        for (i, line) in fs::read_to_string(input)?.lines().enumerate() {
+            let mut first = line
+                .find(|c: char| c.is_digit(10))
+                .map(|i| (i, line[i..i + 1].parse::<u8>().unwrap_or(0)))
+                .unwrap_or((line.len(), 0));
+            let mut last = line
+                .rfind(|c: char| c.is_digit(10))
+                .map(|i| (i, line[i..i + 1].parse::<u8>().unwrap_or(0)))
+                .unwrap_or((0, 0));
+
+            for (i, nr) in numbers.iter().enumerate() {
+                let mut found = line.match_indices(nr);
+                if let Some((j, _)) = found.next() {
+                    if j < first.0 {
+                        first = (j, (i + 1) as u8);
+                    } else if j >= last.0 {
+                        last = (j, (i + 1) as u8);
+                    }
+                }
+
+                if let Some((j, _)) = found.last() {
+                    if j >= last.0 {
+                        last = (j, (i + 1) as u8);
+                    }
+                }
+            }
+
+            if first.0 < line.len() {
+                sum += (first.1 * 10 + last.1) as u32;
             } else {
                 eprintln!("Line {}({}) didn't contain a single digit.", i, line);
             }
