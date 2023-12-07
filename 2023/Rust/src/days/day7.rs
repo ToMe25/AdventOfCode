2383c89902181f696a77cf39dd945b2e185e4827
ToMe25 <38815969+ToMe25@users.noreply.github.com>
2023-12-07 18:01:51 +0100

    Implement Day 7 part 2 in Rust

--- 2023/Rust/src/days/day7.rs
+++ 2023/Rust/src/days/day7.rs
@@ -96,6 +96,96 @@ impl DayRunner for Day7Runner {
 
         Ok(Some(result.to_string()))
     }
+
+    fn part2(&self) -> Result<Option<String>, Box<dyn Error>> {
+        let input_data = fs::read_to_string(get_input_file(7)?)?;
+        let cards: HashMap<char, u8> = [
+            'J', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'Q', 'K', 'A',
+        ]
+        .iter()
+        .enumerate()
+        .map(|(i, c)| (*c, i as u8))
+        .collect();
+
+        let hands: Vec<&str> = input_data
+            .lines()
+            .map(|line| line.split_once(&[' ', '\t']).unwrap().0)
+            .collect();
+
+        let mut types: Vec<(&str, HandType)> = hands
+            .iter()
+            .map(|hand| {
+                let mut amounts: HashMap<char, u8> = HashMap::new();
+                hand.chars()
+                    .for_each(|c| *amounts.entry(c).or_insert(0) += 1);
+                amounts
+            })
+            .map(|mut counts| {
+                (
+                    counts.remove(&'J').unwrap_or(0),
+                    counts.into_iter().collect::<Vec<(char, u8)>>(),
+                )
+            })
+            .map(|(js, mut vec)| {
+                if vec.is_empty() {
+                    return vec![5];
+                }
+                vec.sort_by(|a, b| a.1.cmp(&b.1));
+                let i = vec.len() - 1;
+                vec[i].1 += js;
+                vec.iter().map(|(_, nr)| *nr).collect::<Vec<u8>>()
+            })
+            .map(|counts| {
+                if counts[0] == 5 {
+                    HandType::FiveOfAKind
+                } else if counts[1] == 4 {
+                    HandType::FourOfAKind
+                } else if counts[1] == 3 && counts[0] == 2 {
+                    HandType::FullHouse
+                } else if counts[2] == 3 {
+                    HandType::ThreeOfAKind
+                } else if counts[1] == 2 && counts[2] == 2 {
+                    HandType::TwoPair
+                } else if counts[3] == 2 {
+                    HandType::OnePair
+                } else {
+                    HandType::HighCard
+                }
+            })
+            .zip(hands.iter())
+            .map(|(typ, hand)| (*hand, typ))
+            .collect();
+
+        types.sort_by(|(hand0, typ0), (hand1, typ1)| {
+            if typ0.cmp(typ1) != Ordering::Equal {
+                typ0.cmp(typ1)
+            } else {
+                hand0
+                    .chars()
+                    .map(|c| cards.get(&c).unwrap_or(&0))
+                    .zip(hand1.chars().map(|c| cards.get(&c).unwrap_or(&0)))
+                    .map(|(c0, c1)| c0.cmp(&c1))
+                    .filter(|ord| *ord != Ordering::Equal)
+                    .next()
+                    .unwrap_or(Ordering::Equal)
+            }
+        });
+
+        let bets: HashMap<&str, u16> = input_data
+            .lines()
+            .map(|line| (line.split_once(&[' ', '\t']).unwrap()))
+            .map(|(hand, bet)| (hand, bet.parse().unwrap()))
+            .collect();
+
+        let result: u64 = types
+            .iter()
+            .map(|(hand, _)| bets.get(hand).unwrap())
+            .enumerate()
+            .map(|(i, bet)| *bet as u64 * (i + 1) as u64)
+            .sum();
+
+        Ok(Some(result.to_string()))
+    }
 }
 
 /// The various types of hands.
