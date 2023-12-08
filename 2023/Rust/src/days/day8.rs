25a11d065e39c05a551e7b07048a78c48210f0c2
ToMe25 <38815969+ToMe25@users.noreply.github.com>
2023-12-08 17:08:34 +0100

    Attempt to implement part 2
    Should work, but takes way too long

--- 2023/Rust/src/days/day8.rs
+++ 2023/Rust/src/days/day8.rs
@@ -13,16 +13,27 @@ use super::DayRunner;
 ///
 /// The [DayRunner] implementation for the aoc day 8.
 #[derive(Debug, Clone, Default)]
-pub struct Day8Runner {}
+pub struct Day8Runner {
+    /// The instructions for where to walk.
+    ///
+    /// The parsed right/left instructions from the input.  
+    /// `true` means right and `false` means left.
+    instructions: Vec<bool>,
+    /// The nodes from the input file.
+    ///
+    /// The nodes from the input file.  
+    /// The key is the current node, and the value are the possible next nodes.
+    nodes: HashMap<String, (String, String)>,
+}
 
 impl DayRunner for Day8Runner {
-    fn part1(&self) -> Result<Option<String>, Box<dyn Error>> {
+    fn init(&mut self) -> Result<(), Box<dyn Error>> {
         let input_data = fs::read_to_string(get_input_file(8)?)?;
         let mut lines = input_data.lines().filter(|line| !line.is_empty());
 
-        let mut instructions = lines.next().unwrap().chars().map(|c| c == 'R').cycle();
+        self.instructions = lines.next().unwrap().chars().map(|c| c == 'R').collect();
 
-        let nodes: HashMap<&str, (&str, &str)> = lines
+        self.nodes = lines
             .map(|line| line.split_once('=').unwrap())
             .map(|(from, to)| {
                 (
@@ -32,22 +43,53 @@ impl DayRunner for Day8Runner {
                         .unwrap(),
                 )
             })
-            .map(|(from, (tol, tor))| (from.trim(), (tol.trim(), tor.trim())))
+            .map(|(from, (tol, tor))| {
+                (
+                    from.trim().to_owned(),
+                    (tol.trim().to_owned(), tor.trim().to_owned()),
+                )
+            })
             .collect();
 
-        let mut current = "AAA";
+        Ok(())
+    }
+
+    fn part1(&self) -> Result<Option<String>, Box<dyn Error>> {
+        let mut insts = self.instructions.iter().cycle();
+        let mut current = &String::from("AAA");
         let mut steps: usize = 0;
         while current != "ZZZ" {
-            current = nodes
+            current = self
+                .nodes
                 .get(current)
                 .map(|node| {
-                    if instructions.next().unwrap() {
-                        node.1
+                    if *insts.next().unwrap() {
+                        &node.1
                     } else {
-                        node.0
+                        &node.0
                     }
                 })
                 .unwrap();
+            steps += 1;
+        }
+
+        Ok(Some(steps.to_string()))
+    }
+
+    fn part2(&self) -> Result<Option<String>, Box<dyn Error>> {
+        let mut insts = self.instructions.iter().cycle();
+        let mut current: Vec<&String> =
+            self.nodes.keys().filter(|key| key.ends_with('A')).collect();
+        let mut steps: usize = 0;
+        while !current.iter().all(|pos| pos.ends_with('Z')) {
+            let right = *insts.next().unwrap();
+            for i in 0..current.len() {
+                current[i] = self
+                    .nodes
+                    .get(current[i])
+                    .map(|node| if right { &node.1 } else { &node.0 })
+                    .unwrap();
+            }
             steps += 1;
         }
 
