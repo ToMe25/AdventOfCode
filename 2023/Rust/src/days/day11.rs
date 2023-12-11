a9166c8f2aa0630e5b4e1dbefa11436660711d73
ToMe25 <38815969+ToMe25@users.noreply.github.com>
2023-12-11 16:53:05 +0100

    Implement Day 11 part 2 in Rust

--- 2023/Rust/src/days/day11.rs
+++ 2023/Rust/src/days/day11.rs
@@ -13,49 +13,120 @@ use super::DayRunner;
 ///
 /// The [DayRunner] implementation for the aoc day 11.
 #[derive(Debug, Clone, Default)]
-pub struct Day11Runner {}
+pub struct Day11Runner {
+    /// The galaxies from the input data.
+    ///
+    /// The galaxies as parsed from the input data, without any space expansion.
+    galaxies: Vec<Galaxy>,
+    /// The empty rows from the input data.
+    ///
+    /// The rows from the input data that are filled with only dots.  
+    /// Zero indexed.
+    empty_rows: Vec<usize>,
+    /// The empty columns from the input data.
+    ///
+    /// The colums from the input data that are filled with only dots.  
+    /// Zero indexed.
+    empty_cols: Vec<usize>,
+}
 
 impl DayRunner for Day11Runner {
-    fn part1(&self) -> Result<Option<String>, Box<dyn Error>> {
+    fn init(&mut self) -> Result<(), Box<dyn Error>> {
         let input_data = fs::read_to_string(get_input_file(11)?)?;
-        let mut map: Vec<Vec<char>> = input_data
+        let input_lines: Vec<&str> = input_data
             .lines()
-            .map(|line| line.chars().collect())
+            .filter(|line| !line.trim().is_empty())
             .collect();
 
-        let mut inserted: usize = 0;
-        for y in 0..map.len() {
-            let empty = map[y + inserted].iter().all(|c| c == &'.');
-            if empty {
-                map.insert(
-                    y + inserted,
-                    (0..map[y + inserted].len()).map(|_| '.').collect(),
-                );
-                inserted += 1;
-            }
-        }
-
-        inserted = 0;
-        // Assume the map is rectangular
-        for x in 0..map[0].len() {
-            let empty = map.iter().map(|line| line[x + inserted]).all(|c| c == '.');
-            if empty {
-                map.iter_mut().for_each(|line| {
-                    line.insert(x + inserted, '.');
-                });
-                inserted += 1;
-            }
-        }
-
-        let galaxies: Vec<Galaxy> = map
+        self.galaxies = input_lines
             .iter()
             .enumerate()
             .flat_map(|(y, line)| {
-                line.iter()
+                line.chars()
                     .enumerate()
-                    .filter(|(_, c)| *c == &'#')
+                    .filter(|(_, c)| c == &'#')
                     .map(|(x, _)| Galaxy { x_pos: x, y_pos: y })
                     .collect::<Vec<Galaxy>>()
+            })
+            .collect();
+
+        self.empty_rows = input_lines
+            .iter()
+            .enumerate()
+            .filter(|(_, line)| line.trim().chars().all(|c| c == '.'))
+            .map(|(y, _)| y)
+            .collect();
+
+        self.empty_cols = (0..input_lines[0].len())
+            .filter(|col| {
+                input_lines
+                    .iter()
+                    .map(|line| line.chars().nth(*col).unwrap())
+                    .all(|c| c == '.')
+            })
+            .collect();
+
+        Ok(())
+    }
+
+    fn part1(&self) -> Result<Option<String>, Box<dyn Error>> {
+        let galaxies: Vec<Galaxy> = self
+            .galaxies
+            .iter()
+            .map(|galaxy| {
+                let offset_x = self
+                    .empty_cols
+                    .iter()
+                    .filter(|col| **col < galaxy.x_pos)
+                    .count();
+                let offset_y = self
+                    .empty_rows
+                    .iter()
+                    .filter(|row| **row < galaxy.y_pos)
+                    .count();
+                Galaxy {
+                    x_pos: galaxy.x_pos + offset_x,
+                    y_pos: galaxy.y_pos + offset_y,
+                }
+            })
+            .collect();
+
+        let mut checked: HashSet<Galaxy> = HashSet::new();
+        let distance_sum: usize = galaxies
+            .clone()
+            .iter()
+            .flat_map(|galaxy| {
+                checked.insert(*galaxy);
+                galaxies
+                    .iter()
+                    .filter(|gal| !checked.contains(gal))
+                    .map(|gal| galaxy.manhatten_distance_to(gal))
+                    .collect::<Vec<usize>>()
+            })
+            .sum();
+
+        Ok(Some(distance_sum.to_string()))
+    }
+
+    fn part2(&self) -> Result<Option<String>, Box<dyn Error>> {
+        let galaxies: Vec<Galaxy> = self
+            .galaxies
+            .iter()
+            .map(|galaxy| {
+                let offset_x = self
+                    .empty_cols
+                    .iter()
+                    .filter(|col| **col < galaxy.x_pos)
+                    .count();
+                let offset_y = self
+                    .empty_rows
+                    .iter()
+                    .filter(|row| **row < galaxy.y_pos)
+                    .count();
+                Galaxy {
+                    x_pos: galaxy.x_pos + offset_x * 999999,
+                    y_pos: galaxy.y_pos + offset_y * 999999,
+                }
             })
             .collect();
 
