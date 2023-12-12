be44c672a2394369f50ab8a605f15c37468b647a
ToMe25 <38815969+ToMe25@users.noreply.github.com>
2023-12-12 23:06:14 +0100

    Attempt implementing part 2
    It currently takes way too much RAM to finish on my PC.

--- 2023/Rust/src/days/day12.rs
+++ 2023/Rust/src/days/day12.rs
@@ -18,152 +18,173 @@ impl DayRunner for Day12Runner {
     fn part1(&self) -> Result<Option<String>, Box<dyn Error>> {
         let input_data = fs::read_to_string(get_input_file(12)?)?;
 
-        let mut sum: usize = 0;
-        for line in input_data.lines() {
-            let (map_line, lengths) = line.split_once(' ').unwrap();
-            let states: Vec<SpringState> = map_line
-                .chars()
-                .map(|c| match c {
-                    '.' => SpringState::Operational,
-                    '?' => SpringState::Unknown,
-                    '#' => SpringState::Damaged,
-                    x => panic!("Received unknown spring state '{}'!", x),
-                })
-                .collect();
-
-            let mut possibilities: Vec<Possibilities> = lengths
-                .split(',')
-                .map(|nr| nr.parse::<u8>().unwrap())
-                .map(|nr| Possibilities {
-                    length: nr,
-                    positions: Vec::new(),
-                })
-                .collect();
-
-            for i in 0..possibilities.len() {
-                let length = possibilities[i].length;
-                let start;
-                if i == 0 {
-                    start = 0;
-                } else {
-                    start = possibilities[i - 1].positions[0]
-                        + possibilities[i - 1].length as usize
-                        + 1;
-                }
-
-                let mut positions: Vec<usize> = Vec::new();
-                for pos in start..=(states.len() - length as usize) {
-                    if pos > 0 && states[pos - 1] == SpringState::Damaged {
-                        continue;
-                    }
-
-                    if pos + length as usize > states.len() {
-                        break;
-                    }
-
-                    if (pos..pos + length as usize)
-                        .map(|i| &states[i])
-                        .any(|state| state == &SpringState::Operational)
-                    {
-                        continue;
-                    }
-
-                    if pos + length as usize == states.len()
-                        || states[pos + length as usize] != SpringState::Damaged
-                    {
-                        positions.push(pos);
-                    }
-                }
-
-                possibilities[i].positions = positions;
-            }
-
-            for i in (0..possibilities.len() - 1).rev() {
-                let end = *possibilities[i + 1].positions.last().unwrap();
-                let length = possibilities[i].length as usize;
-                possibilities[i].positions = possibilities[i]
-                    .positions
-                    .iter()
-                    .filter(|pos| *pos + length < end)
-                    .copied()
-                    .collect();
-            }
-
-            /*possibilities = possibilities
-                .into_iter()
-                .inspect(|poss| {
-                    assert!(
-                        !poss.positions.is_empty(),
-                        "Found no possible result for line \"{}\"",
-                        line
-                    )
-                })
-                .filter(|poss| poss.positions.len() > 1)
-                .collect();
-            if possibilities.is_empty() {
-                sum += 1;
-                continue;
-            }*/
-
-            let mut combis: Vec<Vec<(u8, usize)>> = Vec::new();
-            combis.push(Vec::new());
-            for poss in &possibilities {
-                combis = combis
-                    .iter()
-                    .flat_map(|combi| {
-                        poss.positions
-                            .iter()
-                            .map(|pos| (poss.length, *pos))
-                            .map(|pair| {
-                                let mut cmb = combi.clone();
-                                cmb.push(pair);
-                                cmb
-                            })
-                            .collect::<Vec<Vec<(u8, usize)>>>()
-                    })
-                    .collect();
-            }
-
-            combis = combis
-                .into_iter()
-                .filter(|combi| {
-                    if combi.is_empty() {
-                        return false;
-                    }
-
-                    let mut last: (u8, usize) = combi[0];
-                    for (len, pos) in combi.iter().skip(1) {
-                        if last.1 + last.0 as usize >= *pos {
-                            return false;
-                        }
-                        last = (*len, *pos);
-                    }
-
-                    true
-                })
-                .collect();
-
-            combis = combis
-                .into_iter()
-                .filter(|combi| {
-                    states
-                        .iter()
-                        .enumerate()
-                        .filter(|(_, &state)| state == SpringState::Damaged)
-                        .map(|(i, _)| i)
-                        .all(|i| {
-                            combi
-                                .iter()
-                                .any(|(len, pos)| &i >= pos && i < pos + *len as usize)
-                        })
-                })
-                .collect();
-
-            sum += combis.len();
-        }
+        let sum: usize = input_data
+            .lines()
+            .filter(|line| !line.is_empty())
+            .map(|line| line.split_once(' ').unwrap())
+            .map(|(map_line, lengths)| count_possibilities(map_line, lengths))
+            .sum();
 
         Ok(Some(sum.to_string()))
     }
+
+    fn part2(&self) -> Result<Option<String>, Box<dyn Error>> {
+    	// FIXME find a solution that takes a feasible amount of RAM.
+        let input_data = fs::read_to_string(get_input_file(12)?)?;
+
+        let sum: usize = input_data
+            .lines()
+            .filter(|line| !line.is_empty())
+            .map(|line| line.split_once(' ').unwrap())
+            .map(|(map_line, lengths)| {
+                (
+                    (0..5).map(|_| map_line).collect::<Vec<&str>>().join("?"),
+                    (0..5).map(|_| lengths).collect::<Vec<&str>>().join(","),
+                )
+            })
+            .map(|(map_line, lengths)| count_possibilities(&map_line, &lengths))
+            .sum();
+
+        Ok(Some(sum.to_string()))
+    }
+}
+
+/// Counts the number of possible solutions to the given input data.
+///
+/// Counts the number of possible arrangements of the given broken hot springs in the given data.
+///
+/// # Panics
+///
+/// This function panics if `map_line` includes a character that isn't `.`, `?`, or `#`.
+/// This function also panics if there is no valid solution for the given data.
+fn count_possibilities(map_line: &str, lengths: &str) -> usize {
+    let states: Vec<SpringState> = map_line
+        .chars()
+        .map(|c| match c {
+            '.' => SpringState::Operational,
+            '?' => SpringState::Unknown,
+            '#' => SpringState::Damaged,
+            x => panic!("Received unknown spring state '{}'!", x),
+        })
+        .collect();
+
+    let mut possibilities: Vec<Possibilities> = lengths
+        .split(',')
+        .map(|nr| nr.parse::<u8>().unwrap())
+        .map(|nr| Possibilities {
+            length: nr,
+            positions: Vec::new(),
+        })
+        .collect();
+
+    for i in 0..possibilities.len() {
+        let length = possibilities[i].length;
+        let start;
+        if i == 0 {
+            start = 0;
+        } else {
+            start = possibilities[i - 1].positions[0] + possibilities[i - 1].length as usize + 1;
+        }
+
+        let mut positions: Vec<usize> = Vec::new();
+        for pos in start..=(states.len() - length as usize) {
+            if pos > 0 && states[pos - 1] == SpringState::Damaged {
+                continue;
+            }
+
+            if pos + length as usize > states.len() {
+                break;
+            }
+
+            if (pos..pos + length as usize)
+                .map(|i| &states[i])
+                .any(|state| state == &SpringState::Operational)
+            {
+                continue;
+            }
+
+            if pos + length as usize == states.len()
+                || states[pos + length as usize] != SpringState::Damaged
+            {
+                positions.push(pos);
+            }
+        }
+
+        possibilities[i].positions = positions;
+    }
+
+    for i in (0..possibilities.len() - 1).rev() {
+        let end = *possibilities[i + 1].positions.last().unwrap();
+        let length = possibilities[i].length as usize;
+        possibilities[i].positions = possibilities[i]
+            .positions
+            .iter()
+            .filter(|pos| *pos + length < end)
+            .copied()
+            .collect();
+    }
+
+    let mut combis: Vec<Vec<(u8, usize)>> = Vec::new();
+    combis.push(Vec::new());
+    for (i, poss) in possibilities.iter().enumerate() {
+        let last = i == possibilities.len() - 1;
+        combis = combis
+            .into_iter()
+            .flat_map(|combi| {
+                poss.positions
+                    .iter()
+                    .map(|pos| (poss.length, *pos))
+                    .map(|pair| {
+                        let mut cmb = combi.clone();
+                        cmb.push(pair);
+                        cmb
+                    })
+                    .collect::<Vec<Vec<(u8, usize)>>>()
+            })
+            // Filter out combis with overlapping damaged blocks.
+            .filter(|combi| {
+                if combi.is_empty() {
+                    return false;
+                }
+
+                let mut last: (u8, usize) = combi[0];
+                for (len, pos) in combi.iter().skip(1) {
+                    if last.1 + last.0 as usize >= *pos {
+                        return false;
+                    }
+                    last = (*len, *pos);
+                }
+
+                true
+            })
+            // Filter out combis that miss some damaged springs.
+            .filter(|combi| {
+                states
+                    .iter()
+                    .enumerate()
+                    .take(if last {
+                        states.len()
+                    } else {
+                        combi.last().unwrap().1 + combi.last().unwrap().0 as usize
+                    })
+                    .filter(|(_, &state)| state == SpringState::Damaged)
+                    .map(|(i, _)| i)
+                    .all(|i| {
+                        combi
+                            .iter()
+                            .any(|(len, pos)| &i >= pos && i < pos + *len as usize)
+                    })
+            })
+            .collect();
+    }
+
+    assert!(
+        !combis.is_empty(),
+        "Line {} {} didn't have a valid solution.",
+        map_line,
+        lengths
+    );
+    combis.len()
 }
 
 /// A struct containing the possible positions and length of a block of damaged springs.
