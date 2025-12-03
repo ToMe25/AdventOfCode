0a2b5f684c896590a5ed0eff27a663f6a58e5444
ToMe25 <ToMe25@gmx.de>
2025-12-03 17:08:03 +0100

    Implement 2025 Day 1 Part 2

--- 2025/Rust/src/days/day1.rs
+++ 2025/Rust/src/days/day1.rs
@@ -4,7 +4,7 @@
 
 use std::error::Error;
 
-use crate::dayrunner::{DayRunner, DayRunnerDate, Part1Runner, RunContext};
+use crate::dayrunner::{DayRunner, DayRunnerDate, Part1Runner, Part2Runner, RunContext};
 
 /// The day [Advent of Code 2025](https://adventofcode.com/2025) runner.
 pub struct Day1Runner {
@@ -55,6 +55,43 @@ impl Part1Runner for Day1Runner {
             })
             .filter(|&pos| pos == 0)
             .count();
+
+        Ok(result.to_string())
+    }
+}
+
+impl Part2Runner for Day1Runner {
+    fn part2(&self, _: &RunContext) -> Result<String, Box<dyn Error>> {
+        let result = self
+            .steps
+            .iter()
+            .map(|line| line.split_at(1))
+            .map(|(dir, dist)| {
+                (
+                    dir,
+                    dist.parse::<i16>().expect("Failed to parse input distance"),
+                )
+            })
+            .scan((50i16, 0u16), |(pos, count), (dir, dist)| {
+                let mut new_pos = match dir {
+                    "R" => *pos + dist,
+                    "L" => *pos - dist,
+                    dir => panic!("Received invalid input direction '{}'", dir),
+                };
+                let mut new_count = *count;
+                new_count += (new_pos / 100).unsigned_abs();
+                if new_pos == 0 && pos != &0 {
+                    new_count += 1;
+                } else if new_pos < 0 && pos != &0 {
+                    new_count += 1;
+                }
+                new_pos = (new_pos % 100 + 100) % 100;
+                *pos = new_pos;
+                *count = new_count;
+                Some(new_count)
+            })
+            .last()
+            .expect("Didn't receive any input");
 
         Ok(result.to_string())
     }
